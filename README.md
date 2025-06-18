db4131d261a27c9a3301d1352910f42cf7214e52
github <learner_yu@163.com>
2025-06-18 22:17:55 +0800

    更新 README.md

--- README.md
+++ README.md
@@ -19,4 +19,44 @@
 本项目是一个业余项目，如果存在bug，或者有一些好的点子，可以给我提issue，有时间的话，会更新。也欢迎共创。
 
 ## 快速开始
-// todo
+本项目没有UI，只能配置好环境后，执行python脚本。
+1. 安装python环境
+自行下载python，我用的是python-3.12。或者使用conda安装虚拟环境。
+2. 安装依赖
+```shell
+git clone https://github.com/jeffRao/duplicate_image_detection
+cd duplicate_image_detection
+pip install -r requirements.txt
+```
+3. 执行脚本
+直接执行根目录下的`main.py`即可。
+有两种方式执行，在开发环境，直接执行。执行时修改输入输出目录即可。
+- 直接命令行形式执行
+```shell
+python main.py --input_dir=/path/to/input/dir --output_dir=/path/to/output/dir  // 默认复制图片到新目录
+python main.py --input_dir=/path/to/input/dir --output_dir=/path/to/output/dir --process_mode=move  // 移动图片到新目录
+```
+
+- 开发模式执行
+如果你是在ide中运行项目，你可以修改`main.py`中的`if __name__ == '__main__':`下的代码，然后直接在ide中运行`main.py`文件。
+例如：
+```python
+if __name__ == '__main__':
+    main(input_dir='/path/to/input/dir', output_dir='/path/to/output/dir', process_type='move')
+```
+
+## 细节说明
+- `process_type`支持移动和复制两种格式，默认是复制。如果想保留原始文件，可以使用复制模式，否则可以使用移动模式。
+- 项目执行后，会在`output_dir`目录下生成一个`processed`和`remove`两个文件夹，`processed`文件夹里面是处理后的图片，`remove`文件夹里面是重复的删除的图片。
+- 文件读取时，按照图片的创建时间排序的。
+- 图片名称是按照一定的规则设置的，相似的图片会放在相近的位置。
+- 图片特征提取支持 `convnext`，`mobilenet`，`squeezenet` 共5中模型，工程中提供了`mobilenet_v3_large`和`mobilenet_v3_small`两个权重文件。
+  如果需要使用其他模型，请自行下载。
+  [ConvNeXt](https://github.com/facebookresearch/ConvNeXt)
+  [squeezenet](https://github.com/forresti/SqueezeNet)
+- 如果需要修改使用的模型，则调整`image_process`下面的如下代码参数。
+  ```python
+  # 将第二个参数调整为其他值，具体的，可以参照constant中的变量枚举
+  vectors = cnn_encoding(image_file, constant.MOBILENET_LARGE_MODEL)
+  ```
+- 图片相似度计算使用的是余弦相似度，目前判断重复图片，使用的阈值为0.95，判断相似图片，使用的阈值为0.7。如果你想调整阈值，请查看`image_detect.py`文件中的相关代码。
--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 import os.path
-
+import argparse
 from service.image_detect import detect_image
 from service.image_process import process_image
 
-if __name__ == '__main__':
-    image_path = r'F:\照片备份'
-    output_path = r'F:\照片整理\2024'
-    process_type = 'copy' # move or copy, move 图片移动到output_path中，copy图片复制到output_path中
 
-    processed_image_list = process_image(image_path)
+def main_process(input_path, output_path, oper_type):
+    processed_image_list = process_image(input_path)
     print(f'process image successfully. image count: {len(processed_image_list)}')
 
-    detect_image(processed_image_list, output_path, process_type)
+    detect_image(processed_image_list, output_path, oper_type)
     print(f'process image successfully.')
     successful_dir = os.path.join(output_path, 'processed')
     # 计算successful_dir下的文件数量
-    successful_image_count = len([name for name in os.listdir(successful_dir) if os.path.isfile(os.path.join(successful_dir, name))])
+    successful_image_count = len(
+        [name for name in os.listdir(successful_dir) if os.path.isfile(os.path.join(successful_dir, name))])
     print(f'process image successfully count: {successful_image_count}')
     remove_dir = os.path.join(output_path, 'remove')
     # 计算successful_dir下的文件数量
-    remove_image_count = len([name for name in os.listdir(remove_dir) if os.path.isfile(os.path.join(remove_dir, name))])
+    remove_image_count = len(
+        [name for name in os.listdir(remove_dir) if os.path.isfile(os.path.join(remove_dir, name))])
     print(f'remove image successfully count: {remove_image_count}')
+
+
+if __name__ == '__main__':
+
+    # 创建 ArgumentParser 对象
+    parser = argparse.ArgumentParser(description="Process some integers.")
+
+    # 添加 --work_path 参数
+    parser.add_argument('--input_dir', type=str, required=True, default=r'F:\照片备份', help='Specify the input dir')
+    parser.add_argument('--output_dir', type=str, required=True, default=r'F:\照片整理\2024', help='Specify the output dir')
+    parser.add_argument('--process_type', type=str, default='copy', help='Specify the process type')
+
+    # 解析命令行参数
+    args = parser.parse_args()
+    input_dir = r'F:\照片备份'
+    output_dir = r'F:\照片整理\2024'
+    process_type = 'copy' # move or copy, move 图片移动到 output_dir 中，copy图片复制到 output_dir 中
+
+    main_process(input_dir, output_dir, process_type)
